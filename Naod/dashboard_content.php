89765568200e01d106f90ee6b555cb6feacd8dd1
lakucjohn <lakucjohn@gmail.com>
2018-05-29 17:07:30 +0300

    uploaded from sbtechcomp

--- Naod/dashboard_content.php
+++ Naod/dashboard_content.php
@@ -4,7 +4,7 @@
 
 
     <div class="col-lg-3 col-sm-6">
-        <a href="doctor">
+        <a href="" onclick="">
             <section class="panel">
                 <div class="symbol terques">
                     <i class="fa fa-stethoscope"></i>
@@ -12,12 +12,13 @@
                 <div class="value">
                     <h1 class="">
                         8                                </h1>
-                    <p>Doctor</p>
+                    <p>Cash Sale Receipts</p>
                 </div>
             </section>
-        </a></div><a href="doctor">
-    </a><div class="col-lg-3 col-sm-6"><a href="doctor">
-        </a><a href="patient">
+        </a></div><a href="">
+    </a>
+    <div class="col-lg-3 col-sm-6"><a href="">
+        </a><a href="" onclick="">
             <section class="panel">
                 <div class="symbol blue">
                     <i class="fa fa-users"></i>
@@ -25,41 +26,13 @@
                 <div class="value">
                     <h1 class="">
                         49                                </h1>
-                    <p>Patient</p>
+                    <p>Outgoing Invoices</p>
                 </div>
             </section>
         </a>
     </div>
     <div class="col-lg-3 col-sm-6">
-        <a href="pharmacist">
-            <section class="panel">
-                <div class="symbol terques">
-                    <i class="fa  fa-medkit"></i>
-                </div>
-                <div class="value">
-                    <h1 class="">
-                        2                                </h1>
-                    <p>Cashier</p>
-                </div>
-            </section>
-        </a>
-    </div>
-    <div class="col-lg-3 col-sm-6">
-        <a href="patient/caseList">
-            <section class="panel">
-                <div class="symbol blue">
-                    <i class="fa fa-medkit"></i>
-                </div>
-                <div class="value">
-                    <h1 class="">
-                        42                                </h1>
-                    <p>Patients History</p>
-                </div>
-            </section>
-        </a>
-    </div>
-    <div class="col-lg-3 col-sm-6">
-        <a href="finance/payment">
+        <a href="" onclick="">
             <section class="panel">
                 <div class="symbol blue">
                     <i class="fa fa-money"></i>
@@ -67,13 +40,41 @@
                 <div class="value">
                     <h1 class="">
                         157                                </h1>
-                    <p>Payment Invoice</p>
+                    <p>Proforma Invoices Received</p>
                 </div>
             </section>
         </a>
     </div>
     <div class="col-lg-3 col-sm-6">
-        <a href="medicine">
+        <a href="" onclick="">
+            <section class="panel">
+                <div class="symbol terques">
+                    <i class="fa  fa-medkit"></i>
+                </div>
+                <div class="value">
+                    <h1 class="">
+                        2                                </h1>
+                    <p>Incoming Receipts</p>
+                </div>
+            </section>
+        </a>
+    </div>
+    <div class="col-lg-3 col-sm-6">
+        <a href="" onclick="">
+            <section class="panel">
+                <div class="symbol blue">
+                    <i class="fa fa-medkit"></i>
+                </div>
+                <div class="value">
+                    <h1 class="">
+                        42                                </h1>
+                    <p>Incoming Invoices</p>
+                </div>
+            </section>
+        </a>
+    </div>
+    <div class="col-lg-3 col-sm-6">
+        <a href="" onclick="">
             <section class="panel">
                 <div class="symbol blue">
                     <i class="fa fa-medkit"></i>
@@ -81,27 +82,14 @@
                 <div class="value">
                     <h1 class="">
                         17                                </h1>
-                    <p>Medicine</p>
+                    <p>Proforma Invoices Sent</p>
                 </div>
             </section>
         </a>
     </div>
-    <div class="col-lg-6 col-sm-6">
-        <a href="finance/payment">
-            <section class="panel">
-                <div class="symbol terques">
-                    <i class="fa fa-bar-chart-o"></i>
-                </div>
-                <div class="value">
-                    <h1 class=" count14">
-                        BDT                                        405,273.00                                    </h1>
-                    <p>Total payment</p>
-                </div>
-            </section>
-        </a>
-    </div>
+
     <div class="col-lg-3 col-sm-6">
-        <a href="department">
+        <a href="" onclick="">
             <section class="panel">
                 <div class="symbol blue">
                     <i class="fa fa-dashboard"></i>
@@ -109,11 +97,9 @@
                 <div class="value">
                     <h1 class="">
                         22                                </h1>
-                    <p>Departments</p>
+                    <p>Reports</p>
                 </div>
             </section>
         </a>
     </div>
-</div>
-
 </div>
